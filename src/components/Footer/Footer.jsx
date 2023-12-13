e9e02c4a4a6406a9c000d29e701c620a324719b2
Jignesh <jignesh.sharma.707@gmail.com>
2023-12-13 17:12:39 +0530

    added: Footer component design and made it responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 import styles from './footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 const Footer = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>SummaryBlogs</h1>
+        </div>
+
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+          iure, ad, dolorem quaerat quia a deserunt quibusdam facilis vero, quos
+          esse aliquid! Nesciunt at, in nostrum voluptatibus consequatur,
+          quaerat commodi .
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/'}>Blog</Link>
+          <Link href={'/'}>About</Link>
+          <Link href={'/'}>Contact</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href={'/'}>Style</Link>
+          <Link href={'/'}>Fashion</Link>
+          <Link href={'/'}>Coding</Link>
+          <Link href={'/'}>Travel</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href={'/'}>Facebook</Link>
+          <Link href={'/'}>Instagram</Link>
+          <Link href={'/'}>Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
