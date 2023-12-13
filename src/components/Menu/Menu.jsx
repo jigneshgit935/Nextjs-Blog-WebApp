594947043dc6a9ced9ab82a8d0dea09fade6ab8f
Jignesh <jignesh.sharma.707@gmail.com>
2023-12-13 16:39:37 +0530

    added: Side menu component design

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import styles from './menu.module.css';
+import Link from 'next/link';
+import MenuPosts from '../menuPosts/menuPosts';
+import MenuCategories from '../menuCategories/MenuCategories';
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What&apos;s hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Choosen by the editor</h2>
+      <h1 className={styles.title}>Editor pick</h1>
+      <MenuPosts withImage={true} />
+    </div>
+  );
 };
 
 export default Menu;
